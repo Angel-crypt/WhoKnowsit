93320d6d8c98e0a4036a89b44e9bb5cde4ef6482
Crypt <72517634+Angel-crypt@users.noreply.github.com>
2025-12-03 20:35:22 -0600

    feat(data): enhance Question model with properties for trivia game

--- app/src/main/java/com/example/whoknowsit/data/models/Question.kt
+++ app/src/main/java/com/example/whoknowsit/data/models/Question.kt
@@ -1,4 +1,13 @@
 package com.example.whoknowsit.data.models
 
-class Question {
-}
+import com.example.whoknowsit.core.Category
+import com.example.whoknowsit.core.Difficulty
+
+data class Question (
+    val id: Int,
+    val text: String,
+    val options: List<String>,
+    val correctAnswerIndex: Int,
+    val category: Category,
+    val difficulty: Difficulty
+)
