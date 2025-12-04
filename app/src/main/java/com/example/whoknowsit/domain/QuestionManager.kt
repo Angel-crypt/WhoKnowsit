11e864416541904600a0297d91d479de8d60126e
Crypt <72517634+Angel-crypt@users.noreply.github.com>
2025-12-03 23:58:02 -0600

    feat(domain): enhance QuestionManager to filter and shuffle questions by category and difficulty

--- app/src/main/java/com/example/whoknowsit/domain/QuestionManager.kt
+++ app/src/main/java/com/example/whoknowsit/domain/QuestionManager.kt
@@ -5,14 +5,23 @@ import com.example.whoknowsit.core.Difficulty
 import com.example.whoknowsit.data.QuestionDataSource
 import com.example.whoknowsit.data.models.Question
 
-class QuestionManager (private val dataSource: QuestionDataSource) {
+class QuestionManager(private val dataSource: QuestionDataSource) {
+
     fun getQuestionsForCategory(
         category: Category,
         difficulty: Difficulty,
         totalQuestions: Int
-    ){}
+    ): List<Question> {
+        val filteredQuestions = if (category == Category.RANDOM) {
+            dataSource.loadQuestions().filter { it.difficulty == difficulty }
+        } else {
+            dataSource.loadQuestions().filter {
+                it.category == category && it.difficulty == difficulty
+            }
+        }
 
-    fun shuffleQuestions(
-        questions: List<Question>
-    ){}
+        return filteredQuestions.shuffled().take(totalQuestions)
+    }
+
+    fun shuffleQuestions(questions: List<Question>): List<Question> = questions.shuffled()
 }
