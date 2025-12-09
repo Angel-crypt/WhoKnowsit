e892cb20fac44cfe207626bb57fd75985a17f824
Crypt <72517634+Angel-crypt@users.noreply.github.com>
2025-12-09 08:46:44 -0600

    feat(core): add serialization support to GameMode enum

--- app/src/main/java/com/example/whoknowsit/core/enums/GameMode.kt
+++ app/src/main/java/com/example/whoknowsit/core/enums/GameMode.kt
@@ -1,5 +1,7 @@
 package com.example.whoknowsit.core.enums
+import kotlinx.serialization.Serializable
 
+@Serializable
 enum class GameMode(val displayName: String) {
     NEW("Nuevo juego"),
     CONTINUE("Continuar")
